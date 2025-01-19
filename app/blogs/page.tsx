97a27f43839ac057ace8d8796c983326ee33e549
jei3m <virozzegt@gmail.com>
2025-01-20 06:46:04 +0800

    ref: div wrapping

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import { getPosts } from '@/lib/posts'
 import Header from '@/components/header/Header';
 import BlogsWithSearch from '@/components/blogs-page/BlogsWithSearch';
+import Footer from '@/components/footer/Footer';
 
 
 export default async function Page() {
@@ -9,11 +10,16 @@ export default async function Page() {
 
 
   return (
-    <section className='mx-auto max-w-6xl mt-20 md:mt-28 p-4 md:p-6 lg:p-4 xl:p-0'>
-        <div className="flex flex-col">
-          <Header/>
-          <BlogsWithSearch posts={posts}/>
-        </div>
-    </section>
+    <div>
+      <section className='min-h-screen mx-auto max-w-6xl p-4 md:p-6 lg:p-4 xl:p-0'>
+          <div className="flex flex-col">
+            <Header/>
+            <div className='mt-20 md:mt-28'>
+             <BlogsWithSearch posts={posts}/>
+            </div>
+          </div>
+      </section>
+      <Footer/>
+    </div>
   )
 }
