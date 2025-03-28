5113a7e114b7af0eb9d0f11ef8c2fb6defdfccdb
jei3m <virozzegt@gmail.com>
2025-03-29 07:44:34 +0800

    style: enhances responsiveness for mobile devices

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -28,9 +28,9 @@ function Page() {
 
     return (
         <div className="bg-zinc-950 flex flex-col mx-auto backdrop-blur-sm flex">
-            <div className='mx-auto max-w-6xl '>
+            <div className='mx-auto max-w-6xl'>
                 <Header />
-                <div className="pt-[6rem] relative mx-auto px-2 md:px-4 xl:px-0 flex flex-col items-center justify-center relative pb-20">
+                <div className="pt-[4.5rem] md:pt-[6rem] relative mx-auto px-2 md:px-4 xl:px-0 flex flex-col items-center justify-center relative pb-20">
                     <section id="projects" className="w-full">
                         <Reveal
                             initial={{ opacity: 0, x: -100 }}
