00583a8428e3ebdc830c6436a561675587fefe59
jei3m <virozzegt@gmail.com>
2025-03-28 19:18:49 +0800

    style: padding adjustments

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,18 +3,16 @@ import Header from '@/components/header/Header';
 import BlogsWithSearch from '@/components/blogs-page/BlogsWithSearch';
 import Footer from '@/components/footer/Footer';
 
-
 export default async function Page() {
 
   const posts = await getPosts()
-
 
   return (
     <div>
       <section className='min-h-screen mx-auto max-w-6xl p-4 md:p-6 lg:p-4 xl:p-0'>
           <div className="flex flex-col">
             <Header/>
-            <div className='mt-20 md:mt-28'>
+            <div className='pt-[2rem] md:pt-[4rem] xl:pt-[5rem]'>
              <BlogsWithSearch posts={posts}/>
             </div>
           </div>
